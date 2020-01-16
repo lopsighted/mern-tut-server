e079e49ebf052d2dd33a0e5ad0c0a0db92dbb6dd
Manny <maison.manny@gmail.com>
2020-01-15 23:06:14 -0800

    version 1.0.0

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Keys = require('./Keys/keys');
 const items = require('./routes/api/items');
-
+const path = require('path');
 
 const app = express();
 
@@ -20,6 +20,16 @@ mongoose
     .catch(err => console.log(err));
 
 app.use('/api/items', items);
+
+//serve static assests
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
